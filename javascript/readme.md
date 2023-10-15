0762de31477cb2a0d8b4a610d671e1b48fb4231b
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2023-10-15 15:47:09 +0530

    completed mvp

---
+++ javascript/readme.md
@@ -0,0 +1,5 @@
+the server is in the server folder use
+
+#nodemon serve.js 
+
+run the live server to launch the html files
--- javascript/summary.js
+++ javascript/summary.js
@@ -39,11 +39,80 @@ window.onload= async  ()=>{
                 )
             })).json()
             console.log(summaryresponse)
+            let maincontainer=document.querySelector("#firstcontainer")
+            summaryresponse.data[0].forEach((value)=>{
+                let totalsummarycontainer=document.createElement("div")
+                totalsummarycontainer.className="totalsummarycontainer"
+                //date
+                let date=document.createElement("div")
+                date.className="summarytext"
+                date.textContent=value._id
+                //totalamount
+                let amount=document.createElement("div")
+                amount.className="summarytext"
+                amount.textContent=value.totalAmount
+                //average
+                let average=document.createElement("div")
+                average.className='summarytext'
+                average.textContent=value.dailyAverage
+
+                totalsummarycontainer.append(date,amount,average)
+                maincontainer.appendChild(totalsummarycontainer)
+            })
+
+            let secondmaincontainer=document.querySelector("#secondcontainer")
+            summaryresponse.data[1].forEach((value)=>{
+                let typesummarycontainer=document.createElement("div")
+                typesummarycontainer.className="typesummarycontainer"
+                //date
+                let date=document.createElement("div")
+                date.className="summarytext"
+                date.textContent=value._id.date
+                //type
+                let type=document.createElement("div")
+                type.className="summarytext"
+                type.textContent=value._id.type
+                //totalamount
+                let amount=document.createElement("div")
+                amount.className="summarytext"
+                amount.textContent=value.totalAmount
+                //average
+                let average=document.createElement("div")
+                average.className='summarytext'
+                average.textContent=value.averageAmount
+
+                typesummarycontainer.append(date,type,amount,average)
+                secondmaincontainer.appendChild(typesummarycontainer)
+
+            })
+
+
+            let thirdmaincontainer=document.querySelector("#thirdcontainer")
+            summaryresponse.data[2].forEach((value)=>{
+                let typemaxcontainer=document.createElement("div")
+                typemaxcontainer.className="typemaxcontainer"
+                //date
+                let date=document.createElement("div")
+                date.className="summarytext"
+                date.textContent=value.date
+                //type
+                let type=document.createElement("div")
+                type.className="summarytext"
+                type.textContent=value.type
+                //totalamount
+                let amount=document.createElement("div")
+                amount.className="summarytext"
+                amount.textContent=value.amount
+                
+                typemaxcontainer.append(date,type,amount)
+                thirdmaincontainer.appendChild(typemaxcontainer)
+
+            })
         }
 
         /*
-            while populating the data due not that first index contains the summary for wholeday
-            and second index contains the data aggregation for each type of each day 
+                -->
+ review the data then populate it
         */
     
     }else{
